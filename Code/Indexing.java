568e3d7b8eb5c6d9ce62cf78c79c321ed4a57f34
Berhan Demiralp <demiralpberhan@gmail.com>
2024-04-13 22:53:57 +0300

    Indexing in final version

--- Code/Indexing.java
+++ Code/Indexing.java
@@ -5,27 +5,26 @@ import java.io.*;
 public class Indexing {
 
     public void process(){
-        String passwords = getUnprocessedPasswords();
-        File folder = getFolderWithPassword(passwords);
+
     }
 
-    public String getUnprocessedPasswords() {
+    public void getUnprocessedPasswords() {
         // Klasör yolunu belirtin
-        String folderPath = "src/Unprocessed-Passwords";
+        String sourceFolderPath = "src/Unprocessed-Passwords";
 
         // Klasörü temsil eden File nesnesini oluşturun
-        File folder = new File(folderPath);
+        File sourceFolder = new File(sourceFolderPath);
 
         // Klasördeki dosyaları kontrol edin
-        if (folder.isDirectory()) {
+        if (sourceFolder.isDirectory()) {
             // Klasördeki her dosya için işlem yapın
-            File[] files = folder.listFiles();
+            File[] files = sourceFolder.listFiles();
             for (File file : files) {
                 if (file.isFile() && file.getName().endsWith(".txt")) {
                     try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
                         String line;
                         while ((line = reader.readLine()) != null) {
-                            return line.trim();
+                            getFileWithPassword(line.trim());
                         }
                         System.out.println();
                     } catch (IOException e) {
@@ -36,34 +35,43 @@ public class Indexing {
         } else {
             System.out.println("Belirtilen yol bir klasörü temsil etmiyor.");
         }
-        return null;
     }
 
-    public File getFolderWithPassword(String password){
+    public void getFileWithPassword(String password){
 
-        String folderPath = "src/Index";
+        String sourceFolderPath = "src/Index";
 
-        File folder = new File(folderPath);
+        File sourceFolder = new File(sourceFolderPath);
 
         String key = String.valueOf(password.charAt(0));
 
-        String newPath="src/key";
-
-        if(folder.isDirectory()){
-            File[] files = folder.listFiles();
-            for (File file : files) {
-                if (file.getName().trim().equals(key) && file.isFile()) {
-                    return file;
+        if(sourceFolder.isDirectory()){
+            File[] folders = sourceFolder.listFiles();
+            for (File folder : folders) {
+                if (folder.getName().trim().equals(key) && folder.isFile()) {
+                    String newFilePath = folder.getPath() + File.separator + key + ".txt";
+                    File txtFile = new File(newFilePath);
+                    writePasswordToFile(txtFile,password);
                 }
             }
         }
         else {
-            return new File(newPath);
+            String newFolderPath = sourceFolder + File.separator + key;
+            File folder =  new File(newFolderPath);
+            String newFilePath = folder.getPath() + File.separator + key + ".txt";
+            File txtFile = new File(newFilePath);
+            writePasswordToFile(txtFile,password);
         }
-        return null;
     }
-    public void writePasswordToFile(File folder, String password){
+    public void writePasswordToFile(File file, String password){
+        String filePath = file.getPath();
+        String content = password; // Password|MD5Hash|Sha128|Sha256|source_file_name
 
+        try (FileWriter writer = new FileWriter(filePath,true)) {
+            writer.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
